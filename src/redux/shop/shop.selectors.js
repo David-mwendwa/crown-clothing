e248efd3f920a5b9ed36d1b720f1e7c32659631e
David Mwendwa <klatzdavie19@gmail.com>
2021-05-03 07:54:50 +0300

    After moving directory and shop data into their own reducer, I created respective selectors, collectionsOverview component  and updated corresponding components with new redux flow directory/shop

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,4 +1,12 @@
-import { createSelector } from 'reselect';
+import { createSelector } from 'reselect'; // Used before normalization // Used before normalization
+
+/*const COLLECTION_ID_MAP = {
+  hats: 1,
+  sneakers: 2,
+  jackets: 3,
+  womens: 4,
+  mens: 5,
+}; */
 
 const selectShop = (state) => state.shop;
 
@@ -6,3 +14,17 @@ export const selectCollections = createSelector(
   [selectShop],
   (shop) => shop.collections
 );
+
+export const selectCollectionsForOreview = createSelector(
+  [selectCollections],
+  (collections) => Object.keys(collections).map((key) => collections[key]) // Turning object into an array
+);
+
+export const selectCollection = (collectionUrlParam) =>
+  createSelector(
+    [selectCollections],
+    (collections) => collections[collectionUrlParam] // After data normalization - changing shop collection data into an object with urls param is the key
+    /*collections.find(
+      (collection) => collection.id === COLLECTION_ID_MAP[collectionUrlParam]
+    )*/ // Before normalization
+  );
