55a268692374e9b481818a6650db39ee0c2d6ed5
David Mwendwa <klatzdavie19@gmail.com>
2021-05-07 08:17:15 +0300

    Scraped off the observables pattern used for interphasing firestore library and implemented promise based pattern to the app - shopPage component

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -28,11 +28,16 @@ class ShopPage extends React.Component {
     const { updateCollections } = this.props;
     const collectionRef = firestore.collection('collections');
 
-    collectionRef.onSnapshot(async (snapshot) => {
+    collectionRef.get().then((snapshot) => {
       const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
       updateCollections(collectionsMap);
       this.setState({ loading: false });
     });
+
+    //Native fetch pattern
+    /*fetch('https://firestore.googleapis.com/v1/projects/crown-clothing-db-bd77b/databases/(default)/documents/collections')
+      .then(response => response.json())
+      .then(collections => console.log(collections));*/
   }
 
   render() {
