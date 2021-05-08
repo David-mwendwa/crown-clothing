3cccc3b632bc9281c3ae95fc3f202598f51d5e4b
David Mwendwa <klatzdavie19@gmail.com>
2021-05-08 08:56:27 +0300

    Introduced redux-saga into the app to handle the async actions previously handled by redux-thunk.

--- src/redux/shop/shop.actions.js
+++ src/redux/shop/shop.actions.js
@@ -4,8 +4,6 @@ import {
   FETCH_COLLECTIONS_FAILURE
 } from './shop.types';
 
-import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils';
-
 export const fetchCollectionsStart = () => ({
   type: FETCH_COLLECTIONS_START
 });
@@ -20,16 +18,16 @@ export const fetchCollectionsFailure = errorMessage => ({
   payload: errorMessage
 });
 
-export const fetchCollectionsStartAsync = () => {
-  return dispatch => {
-    const collectionRef = firestore.collection('collections');
-    dispatch(fetchCollectionsStart())
+// export const fetchCollectionsStartAsync = () => {
+//   return dispatch => {
+//     const collectionRef = firestore.collection('collections');
+//     dispatch(fetchCollectionsStart())
 
-    collectionRef.get().then((snapshot) => {
-      const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
-      dispatch(fetchCollectionsSuccess(collectionsMap));
+//     collectionRef.get().then((snapshot) => {
+//       const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+//       dispatch(fetchCollectionsSuccess(collectionsMap));
 
-    }).catch(error =>
-      dispatch(fetchCollectionsFailure(error.message)));
-  }
-}
+//     }).catch(error =>
+//       dispatch(fetchCollectionsFailure(error.message)));
+//   }
+// }
