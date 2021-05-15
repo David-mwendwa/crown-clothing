0f11f8c4bf3fb513205fcadc40fd2cec2eafe2ac
David Mwendwa <klatzdavie19@gmail.com>
2021-05-15 09:59:40 +0300

    Separated the app into client folder, added express server to help handle backend stripe payment.

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
@@ -7,8 +8,19 @@ const StripeCheckoutButton = ({ price }) => {
     'pk_test_51Imu8iKOyrEmScQWPjLK1SnUf9PbG8rllu8jmde1hV3ACQ6Qgmah6LUbIY6ihb89T4drjJvpcopn6V5HMwKvZEef00WiY46IBc';
 
   const onToken = (token) => {
-    console.log(token);
-    alert('PaymentSuccessfull');
+    axios({
+      url: 'payment',
+      method: 'post',
+      data: {
+        amount: priceForStripe,
+        token
+      }
+    }).then(response => {
+      alert('Payment successful')
+    }).catch(error => {
+      console.log('Payment error: ', JSON.parse(error));
+      alert('There was an issue with your payment. Please make sure to use the provided credit card')
+    })
   };
 
   return (
